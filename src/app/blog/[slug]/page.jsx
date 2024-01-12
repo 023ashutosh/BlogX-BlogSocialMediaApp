7767163e80898f43e584afa5907b6205d32a299e
Ashutosh Pratap Singh <023.ashutosh@gmail.com>
2024-01-12 16:39:00 +0530

    Minor : seek 2:54:57

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,43 +1,28 @@
+// "use client"
 import Image from "next/image";
 import styles from "./singlePost.module.css";
 import { getPost } from "@/lib/data";
 import { Suspense } from "react";
 
-// const getData = async (slug) => {
-//   const blogUrl = `https://jsonplaceholder.typicode.com/posts/${slug}`;
-//   const res = await fetch(blogUrl);
-
-//   if (!res.ok) {
-//     throw new error("Something went wrong.");
-//   }
-
-//   return res.json();
-// };
-
-const currentDate = new Date();
-const formattedDate = `${(currentDate.getMonth() + 1)
-  .toString()
-  .padStart(2, "0")}.${currentDate
-  .getDate()
-  .toString()
-  .padStart(2, "0")}.${currentDate.getFullYear()}`;
-
 const SinglePostPage = async ({ params }) => {
   const { slug } = params;
-  // const post = await getData(slug);
   const post = await getPost(slug);
+  console.log(post);
 
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src={post.img}
-          alt=""
-          fill
-        />
-      </div>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image
+            src={post.img}
+            alt=""
+            fill
+          />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <h1 className={styles.title}>
+          {/* Title */}
           {post.title}
         </h1>
         <div className={styles.detail}>
@@ -49,17 +34,20 @@ const SinglePostPage = async ({ params }) => {
             height={50}
             width={50}
           />
-          <Suspense fallback={<div>Loading...</div>}>
+
+          {/* <Suspense fallback={<div>Loading...</div>}>
             <PostUser userId={post.userId} />
-          </Suspense>
+          </Suspense>  */}
 
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>
-              {formattedDate}
+              01.01.2024
+              {/* {post.createdAt} */}
             </span>
           </div>
         </div>
+        <div className={styles.content}>{post.desc}</div>
         <div className={styles.content}>{post.body}</div>
       </div>
     </div>
